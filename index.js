89f30c02d825c6c455754d4cb5a9f0817b84b296
John J Setlock II <johnsetlock@gmail.com>
2022-07-19 15:31:59 -0500

    commit tree 6067cd1c46d2121fdc02d1d9eff8ac541200b658

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity){
+if (activity)
+return ('This Saturday, I want to '+activity)+'!';
+else
+return "This Saturday, I want to roller-skate!";
+
+
+}
+
+//console.log (saturdayFun("hey"));
+
+function mondayWork(activity){
+
+    if (activity)
+    return ('This Monday, I will '+activity)+'.';
+    else
+    return "This Monday, I will go to the office.";
+    
+
+}
+
+
+function wrapAdjective(highlight){
+
+    function emphatic(adjective) {
+        return "You are "+highlight+adjective+highlight+"!";
+    }
+return emphatic;
+
+
+}
